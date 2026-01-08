904aea7e74382daa4eb2b8b4553aca5f498b5884
tinatamago <33127516+tinatamago@users.noreply.github.com>
2026-01-08 15:34:27 +0900

    first commit

---
+++ src/main/java/com/example/pokecadb/PokecadbApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokecadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokecadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokecadbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokecadb/PokecadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokecadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokecadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
